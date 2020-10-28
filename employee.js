34471a85b0af1be126904291ecdaa36b84f10b9b
Paige Burlew <paigeburlew@gmail.com>
2020-10-28 14:27:10 -0500

    added console table feature, wrote prompt function and named other functions

--- employee.js
+++ employee.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var consoleTable = require("console.table");
 
 
 var connection = mysql.createConnection({
@@ -19,5 +20,89 @@ var connection = mysql.createConnection({
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    // createProduct();
+    startTracker();
   });
+
+  //Function that starts inquirer to prompt questions from user.
+function startTracker() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        // list of options included for MVP
+        "View All Employees",
+        "View departments",
+        "View Employees By Department",
+        "View roles",
+        "Add department",
+        "Add role",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+        "EXIT"
+      ]
+    })
+.then(function(answer) {
+  //take the customer's answer
+  //switch case shows what happens depending on what the customer chooses
+    switch (answer.action) {
+    case "View All Employees":
+      viewEmployees();
+      break;
+
+    case "View All Employees By Department":
+      viewEmployeesByDept();
+      break;
+
+    case "View departments":
+      viewDept();
+      break;
+    
+    case "View roles":
+      viewRoles();
+      break;
+
+    case "Add Employee":
+      addEmployee();
+      break;
+  
+    case "Add department":
+      addDept();
+      break;
+    
+    case "Add role":
+      addRole();
+      break;
+
+    case "Remove Employee":
+      removeEmployee();
+      break;
+    
+    case "Update Employee Role":
+      updateEmployeeRole();
+      break;
+    
+    case "Update Employee Manager":
+      updateEmployeeMng();
+      break;
+    
+    case "EXIT":
+      console.log("Thanks for using Employee Tracker! Have a nice day!")
+      process.exit();
+    }
+  });
+}
+
+viewEmployees();
+viewEmployeesByDept();
+viewDept();
+viewRoles();
+addEmployee();
+addDept();
+addRole();
+removeEmployee();
+updateEmployeeRole();
+updateEmployeeMng();
