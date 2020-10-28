9d17b3d923a7f58dd21fd19245b2bffbc75f09f6
Paige Burlew <paigeburlew@gmail.com>
2020-10-28 14:19:40 -0500

    connection is working correctly:

--- employee.js
+++ employee.js
@@ -19,5 +19,5 @@ var connection = mysql.createConnection({
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    createProduct();
+    // createProduct();
   });
