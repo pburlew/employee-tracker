d89a4dee43b4337b38b35320df28313840254deb
Paige Burlew <paigeburlew@gmail.com>
2020-11-06 12:35:55 -0600

    add role and dept

--- employee.js
+++ employee.js
@@ -223,8 +223,88 @@ function addEmployee() {
 
 
 
-addDept();
-addRole();
+//addDept();
+function addDept() {
+  inquirer
+    .prompt([
+    {
+      name: "dept",
+      type: "input",
+      message: "Enter new department's name:"
+    }
+  ])
+  .then(function(answer) {
+    connection.query(
+      "INSERT INTO departments SET ?",
+      {
+        name: answer.dept
+      },
+      function(err) {
+        if (err) throw err;
+        console.log("Department " + answer.dept + " successfully added!");
+        startApp();
+      }
+    );
+  });
+};
+
+//add role
+function addRole() {
+  var deptChoice = [];
+    connection.query("SELECT * FROM departments", function(err, resDept) {
+      if (err) throw err;
+      for (var i = 0; i < resDept.length; i++) {
+        var deptList = resDept[i].name;
+        deptChoice.push(deptList);
+    }
+
+  inquirer
+  .prompt([
+  {
+    name: "title",
+    type: "input",
+    message: "Enter new role's name:"
+  },
+  {
+    name: "salary",
+    type: "number",
+    message: "Enter new role's salary:"
+  },
+  {
+    name: "department_id",
+    type: "rawlist",
+    message: "Select employee's department:",
+    choices: deptChoice
+  }
+])
+.then(function(answer) {
+
+  var chosenDept;
+        for (var i = 0; i < resDept.length; i++) {
+          if (resDept[i].name === answer.department_id) {
+            chosenDept = resDept[i];
+          }
+        };
+
+  connection.query(
+    "INSERT INTO role SET ?",
+    {
+      title: answer.title,
+      salary:answer.salary,
+      department_id: chosenDept.id
+    },
+    function(err) {
+      if (err) throw err;
+      console.log("New role " + answer.title + " successfully added!");
+      startApp();
+    }
+  );
+});
+})
+};
+
+
+
 removeEmployee();
 updateEmployeeRole();
 updateEmployeeMng();
